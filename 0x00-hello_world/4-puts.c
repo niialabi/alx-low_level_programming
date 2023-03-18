e2be326948b8f33398b25defb5dffceb07f41204
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-03-18 21:00:37 +0000

    Hello, Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	char puzz[] = "Programming is like building a multilingual puzzle\n";
+
+	puts(puzz);
+	return (0);
+}
