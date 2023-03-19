9123a19e3bf1d2fb60336168634c3bc97413c90d
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-03-19 21:31:17 +0000

    semicolon bug fixed

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,15 +14,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n < 0)
 	{
-		printf("%d:is negative\n", n);
+		printf("%d is negative\n", n);
 	}
 	else if (n > 0)
 	{
-		printf("%d:is positive\n", n);
+		printf("%d is positive\n", n);
 	}
 	else
 	{
-		printf("0:is zero\n");
+		printf("0 is zero\n");
 	}
 	return (0);
 }
