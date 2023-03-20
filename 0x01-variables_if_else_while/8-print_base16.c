db68245f025e5783973f0f94238a6c9a91fb6e02
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-03-20 19:07:14 +0000

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int o;
+
+	for (i = 49; i <= 57; i++)
+	{
+		putchar(i);
+	}
+	for (o = 97; o <= 102; o++)
+	{
+		putchar(o);
+	}
+	return (0);
+}
