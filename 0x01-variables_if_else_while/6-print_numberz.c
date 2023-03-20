1b49c853b64dddcb8776d60454b8b8abd35e7d20
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-03-20 02:08:38 +0000

    numbezz update

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 /**
- *main - Entry point
- *Print using putchar function
- *Retrun: Always 0 (Success)
+ * main - Entry point
+ * Program that prints all single digit numbers of base 10
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int i;
 
-	for (i=48; i <= 57; i++)
+	for (i = `48; i <= 57; i++)
 	{
 		putchar(i);
 	}
