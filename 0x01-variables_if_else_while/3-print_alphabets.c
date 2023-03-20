132c379fbfd998d3f4e3465ad13eb6bae29ec622
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-03-20 00:09:45 +0000

    alphaBET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * print out lowercase a-z using putchar function
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int o;
+
+	for (i = 97; i <= 122; i++)
+{
+	putchar(i);
+}
+	for (o = 65; o <= 95; o++)
+{
+	putchar(o);
+}
+	putchar ('\n');
+	return (0);
+}
