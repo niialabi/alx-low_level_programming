d1346b127c93e17812f3e474cec40e5291a605a2
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-03-20 01:12:21 +0000

    Numbers programm from scratch

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Program that prints all single digit numbers of base 10
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+	return (0);
+}
