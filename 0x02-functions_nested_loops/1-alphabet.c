6085335bac630ea803c856575f442465a8d30b67
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-03-22 20:44:26 +0000

    lowercase alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * printing small letter alphabets
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		_putchar(i);
+	}
+	return (0);
+}
