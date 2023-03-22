516db7f24f0717606d0f3dba686f853298834749
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-03-22 19:02:09 +0000

    _putchar function added

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * Main - Entry point
+ * Program that prints _putchar
+ *
+ * Return: Always 0 (Success)
+ */
+main(void)
+{
+	putchar(95);
+	putchar(112);
+	putchar(117);
+	putchar(116);
+	putchar(99);
+	putchar(104);
+	putchar(97);
+	putchar(114);
+	return (0);
+}
