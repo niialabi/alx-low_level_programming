f48fe6f3cf04eb54eb2b5933b3d445c413a9e825
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-03-23 12:40:09 +0000

    is upper task

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isupper - Entry point in program
+ * c - parameter allowed
+ * checks if character inputed is upperCase
+ * _isupper : function already declared that checks if upper or else
+ * Return: 1 Success / 0 failed
+ */
+int _isupper(int c)
+{
+	if (_isupper(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	return (0);
+}
