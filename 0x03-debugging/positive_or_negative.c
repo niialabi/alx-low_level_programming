ebff376e7a5fbace1009e6382325ae7cffa790bd
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-03-25 00:55:56 +0000

    Project start

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Radomly generating numbers and printing positive negative or zero
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        int n;
+
+        srand(time(0));
+        n = rand() - RAND_MAX / 2;
+        if (n < 0)
+        {
+                printf("%d is negative\n", n);
+        }
+        else if (n > 0)
+        {
+                printf("%d is positive\n", n);
+        }
+        else
+        {
+                printf("0 is zero\n");
+        }
+        return (0);
+}
+
