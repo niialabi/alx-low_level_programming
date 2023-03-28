9da06cc5f12b668b7d32461ab42061e026fd7f43
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-03-28 03:36:26 +0000

    strlenlike program written

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strlen - Check string length function
+ * @s: pointer to string
+ * Description: checks and returns the lengh of a sting
+ *
+ * Return: returns and integer (Success)
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	len = strlen(s);
+	return (len);
+}
