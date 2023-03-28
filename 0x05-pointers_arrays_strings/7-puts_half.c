7a0397633f5efb228428a9d171da0c7804240c6c
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-03-28 20:23:22 +0000

    print half a string task

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * puts_half - PRINT HALF FUNCTION
+ *
+ * Description: function prints half of any string
+ * @str: pointer to string
+ */
+void puts_half(char *str)
+{
+	int n, len, i;
+
+	len = strlen(str);
+	if (len % 2 == 0)
+	{
+		n = (len / 2);
+	}
+	else
+	{
+		n = (len - 1) / 2;
+	}
+	for (i = 1; i <= n; i++)
+	{
+		_putchar(str[i]);
+	}
+}
