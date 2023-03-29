5a7ba7dc84ce4e2735a4731926282e3e11dcddf1
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-03-29 01:30:57 +0000

    rm excess

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -17,13 +17,9 @@ void print_array(int *a, int n)
 		{
 			printf("%d, ", a[i]);
 		}
-		else if (i == (n - 1))
+		else
 		{
 			printf("%d\n", a[i]);
-		}
-		else if (i < 0)
-		{
-			printf(" \n");
 		}
 	}
 }
