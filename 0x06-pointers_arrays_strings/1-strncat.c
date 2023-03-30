7f095b70f313f5383bde2383f47536a3cad3f7d1
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-03-30 14:06:20 +0000

    strncat function

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ * *_strncat - Main function to join 2 st
+ * @dest: destination string
+ * @src: source string
+ * @n: number in array
+ *
+ * Description: function joins 2 strings
+ * Return: returns new string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	n = strlen(src);
+	strncat(dest, src, n);
+	return (dest);
+}
