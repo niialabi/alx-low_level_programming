899cb21cd238d45a55b0d916029406f871b94d92
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-03-30 12:40:22 +0000

    betty errors fixed

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,27 +2,17 @@
 #include <string.h>
 #include <stdio.h>
 /**
- * _strcat - String concact function
+ * *_strcat - String concact function
  * @dest: destination string
  * @src: source string
- *
+ * does the same thing as strcat
  * Description: Program concactinates 2 strings into 1
- * return: retuns ammended dest (old dest + src)
+ *
+ * Return: destination spring successful
  */
 
 char *_strcat(char *dest, char *src)
 {
-	int length, i;
-
-	length = 0;
-	while (dest[length] != '\0')
-	{
-		++length;
-	}
-	for (i = 0; src[i] != '\0'; ++i)
-	{
-		dest[length] = src[i];
-	}
-	dest[length] = '\0';
+	dest = strcat(dest, src);
 	return (dest);
 }
