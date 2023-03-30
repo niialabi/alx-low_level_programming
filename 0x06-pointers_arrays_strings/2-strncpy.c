ebe56875423d3880ee2d75a0bf095189ac519f7f
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-03-30 16:01:30 +0000

    *_strncpy function copied

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * *_strncpy - Main function that copies one string to another
+ * @dest: destination string
+ * @src: src string
+ * @n: number of characters to copy
+ *
+ * Return: return value is pointer for dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int lensrc;
+
+	lensrc = strlen(src);
+	n = lensrc;
+	strncpy(dest, src, n);
+	return (dest);
+}
