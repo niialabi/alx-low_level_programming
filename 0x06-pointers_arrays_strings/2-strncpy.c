e0cd06ca7128a995bcdc0acad999b6731b0454f6
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-03-30 20:04:46 +0000

    new sytax for problem

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,7 +11,15 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	n = sizeof(src);
-	strncpy(dest, src, n);
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+		for ( ; i < n; i++)
+		{
+			dest[i] = '\0';
+		}
+	}
 	return (dest);
 }
