fb52b5b0429b6a3ab1dd09bc97038f7664c140ec
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-04-02 12:54:50 +0000

    sign task fixed

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,13 +10,13 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		return (1);
 		_putchar(43);
+		return (1);
 	}
 	else if (n < 0)
 	{
-		return (-1);
 		_putchar(45);
+		return (-1);
 	}
 	else if (n == 0)
 	{
