1169afe81a21bce2221b45afde4308a813d2757d
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-04-02 18:52:48 +0000

    last digit task redo

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_last_digit - func print last digit
+ * @a: int to be checked
+ * Description: returns value of last dig.
+ * Return: value of last digit
+ */
+
+int print_last_digit(int a)
+{
+	if (a < 0)
+	{
+		a = (-(a) % 10);
+	}
+	else
+	{
+		a = (a % 10);
+	}
+	_putchar(a);
+	return (a);
+}
