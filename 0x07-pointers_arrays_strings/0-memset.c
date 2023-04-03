60626fa4af674756f0309913c2890c9b8fa2b216
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-04-03 22:05:44 +0000

    *memset function

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * *_memset - Set memory function
+ * @s: pointer to var
+ * @b: variable to set
+ * @n: number of mem allocation
+ * Return: pointer to s variable
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
