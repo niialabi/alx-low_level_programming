f440b7ea0cac5c28701c1ff9c07d44815abe6e43
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-04-07 05:18:47 +0000

    recursion print reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _print_rev_recursion - man function rev string
+ * @s: pointer to org string
+ * Description: functing prints sting in rev
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion (s + 1);
+		_putchar(*s);
+	}
+}
