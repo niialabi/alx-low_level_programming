e451eaa0977f815aba17a8e66ea449891e15d9fe
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-04-10 23:12:18 +0000

    task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - Function that multiplies 2 numbers
+ * @argc: argument count
+ * @argv: arguments to be multipled
+ * Return: 0 Success
+ */
+int main(__attribute__((unused)) int argc,
+		__attribute__((unused)) char *argv[])
+{
+	int  j, i;
+	int sum = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		if (*argv[i] >= 48 && *argv[i] <= 57)
+		{
+			continue;
+		}
+		else
+		{
+			printf("Error\n");
+			return (0);
+		}
+	}
+	for (j = 1; j < argc; j++)
+	{
+		sum += atoi(argv[j]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
