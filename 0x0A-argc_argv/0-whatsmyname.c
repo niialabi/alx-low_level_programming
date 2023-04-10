bc9c9e03e9a02451d81f79e5ef5770a3bf7a1307
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-04-10 08:53:31 +0000

    betty error resolved

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,7 +5,8 @@
  * @argv: argument vector
  * Return: returns 0 if successful
  */
-int main(__attribute__((unused)) int argc, __attribute__((unused)) char *argv[])
+int main(__attribute__((unused)) int argc,
+		__attribute__((unused)) char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
