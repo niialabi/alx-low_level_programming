a067d188e20b2c661c7592813315efd6fd9946f2
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-04-10 09:16:31 +0000

    silence is argument

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - function name
+ * @argc: argument counter
+ * @argv: argument vector
+ * Return: 0 is successful
+ */
+int main(__attribute__((unused)) int argc,
+		__attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
