5f5ca438537bb0ed3c4fdbc0a3a26f1500f64204
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-04-10 08:50:36 +0000

    my name is task

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - main function with args
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: returns 0 if successful
+ */
+int main(__attribute__((unused)) int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
