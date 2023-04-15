13d9009db4e094aaa0d0975ef585d600dcdcdd92
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-04-15 14:09:01 +0000

    free alloc task done

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * free_grid - FUNCTION FREES ALLOC MEMORY
+ * @grid: pointer to grid[0]
+ * @height: roll number in grid
+ *
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	return;
+}
