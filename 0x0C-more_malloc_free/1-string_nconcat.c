51dd522be7f1c06f8e8a2429c6c85750808162e9
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-04-16 18:06:15 +0000

    errors fixed

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,23 +13,26 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, j;
+	unsigned int i, j, l1, l2;
 	char *s3;
-
-	s3 = malloc(sizeof(char) * (strlen(s1) + n));
-		if (s3 == NULL)
-		{
-			return (NULL);
-		}
-	for (i = 0; i < strlen(s1) && *s1 != '\0'; i++)
+	
+	for (l1 = 0; s1 != NULL && s1[l1] != '\0'; l1++)
+		;
+	for (l2 = 0; s2 != NULL && s2[l2] != '\0'; l2++)
+		;
+	if (n > l2)
+		n = l2;
+	s3 = malloc(sizeof(char) * (l1 + n + 1));
+	if (s3 == NULL)
+		return NULL;
+	for (i = 0; i < l1 && s1 != NULL; i++)
 	{
 		s3[i] = s1[i];
 	}
-	for (j = 0; j < n; j++)
+	for (j = 0; j < n && s2 != NULL; j++)
 	{
-		s3[strlen(s1) + j] = s2[j];
-		if (j == n)
-			s3[strlen(s1) + j] = '\0';
+		s3[i + j] = s2[j];
 	}
+	/**s3[n + l1] = '\0';*/
 	return (s3);
 }
