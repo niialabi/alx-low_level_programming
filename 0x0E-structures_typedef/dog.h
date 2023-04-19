faaa1c097941a6acb13861715b380407a1795ce5
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-04-19 19:56:50 +0000

    my dog struct declaration

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef STRUCT_DOG
+#define STRUCT_DOG
+/**
+ * struct dog - dog details
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: owner info
+ *
+ * Description: struct to store basic info of dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+
+
+#endif
