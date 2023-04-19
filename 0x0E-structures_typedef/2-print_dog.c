20048ab5a4c605c2eb2beda53ec48d2b7c694267
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-04-19 22:27:29 +0000

    error try 4

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,11 +9,14 @@
  */
 void print_dog(struct dog *d)
 {
-	if (!d->name)
-		d->name = "(nil)";
-	if (!d->owner)
-		d->owner = "(nil)";
-	printf("Name: %s\n", d->name);
-	printf("Age: %f\n", d->age);
-	printf("Owner: %s\n", d->owner);
+	if (d)
+	{
+		if (!d->name)
+			d->name = "(nil)";
+		if (!d->owner)
+			d->owner = "(nil)";
+		printf("Name: %s\n", d->name);
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", d->owner);
+	}
 }
