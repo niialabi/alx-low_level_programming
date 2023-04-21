8eed2daf6639011d1df8c540322096e7bbd37303
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-04-21 05:26:06 +0000

    new sintax try

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,14 +12,15 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list args;
 	unsigned int i;
-
+	/*const char *sp = separator;*/
+	
 	va_start(args, n);
 	{
 		for (i = 0; i < n; i++)
 		{
 			printf("%d", va_arg(args, int));
-			if (i != n - 1 && *separator != n - 1)
-			printf("%s", separator);
+			if (separator && i != (n - 1))
+				printf("%s", separator);
 		}
 	}
 	printf("\n");
