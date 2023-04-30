b713556fc1dac728ef379cb3b2573f27f9f64261
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-04-30 19:57:19 +0000

    free mem function

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_list - frees alloc memory
+ * @head: pointer to strt of list
+ *
+ */
+void free_list(list_t *head)
+{
+	struct list_s *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp->str);
+		free(tmp);
+	}
+}
