f717478880093956116b8f98e38dfb70ad6b2b9f
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-05-01 19:15:36 +0000

    more singly liked task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * print_listint - prints int lists
+ * @h: lists head
+ *
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	int i = 0;
+	struct listint_s const *ptr;
+	
+	ptr = NULL;
+	if (h == NULL)
+		return (0);
+	ptr = h;
+	while (ptr != NULL)
+	{
+		printf("%d\n", ptr->n);
+		ptr = ptr->next;
+		i++;
+	}
+	return (i);
+}
