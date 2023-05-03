00730306ad3099729a34061751bd66521f993f34
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-05-03 05:08:22 +0000

    delete first node

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include "stdlib.h"
+/**
+ * pop_listint - rmv 1st node
+ * @head: pointer to head
+ * 
+ * Return: head node data
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *temp = NULL;
+	int n_data;
+
+	if (head == NULL)
+		return (0);
+	else
+	{
+		n_data = (*head)->n;
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+	}
+	return (n_data);
+}
