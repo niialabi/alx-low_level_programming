f3c2696f8d7bc6051f2a86436f3753c26b94c734
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-05-03 14:55:04 +0000

    get a nodeint task

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * get_nodeint_at_index - returns no. of elements
+ * @h: pointer to header
+ *
+ * Return: nth node
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+	struct listint_s *temp;
+
+	if (head == NULL)
+		return (0);
+	temp = head;
+	while (temp != NULL)
+	{
+		if (i == index)
+		{
+			break;
+		}
+		temp = temp->next;
+		i++;
+	}
+	return (temp);
+}
