b10efd97b40b1e8b587d4c67bab0e44d1f56d3fe
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-05-04 04:55:56 +0000

    errory work on

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,7 +14,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new_node = NULL;
 	unsigned int i = 0;
 
-	if (!*head)
+	if (!*head && idx)
 		return (NULL);
 	new_node = malloc(sizeof(listint_t));
 	if (!new_node)
