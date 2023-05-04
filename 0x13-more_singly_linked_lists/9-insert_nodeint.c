20c03a780b8142ae0c75bf26ba33066eff19b7c3
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-05-04 05:02:04 +0000

    error fixt attempt 2

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -26,6 +26,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		temp = temp->next;
 		i++;
 	}
+	if (i != idx - 1)
+		return (NULL);
 	new_node->next = temp->next;
 	temp->next = new_node;
 	return (new_node);
