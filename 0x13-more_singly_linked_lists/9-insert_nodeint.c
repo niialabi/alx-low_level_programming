35629a9de41ec67f2f6861cdc6cf0223b85441c6
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-05-04 05:15:33 +0000

    error fix 4

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -28,7 +28,15 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	if (i != idx - 1)
 		return (NULL);
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+	}
+	else
+	{
 	new_node->next = temp->next;
 	temp->next = new_node;
+	}
 	return (new_node);
 }
