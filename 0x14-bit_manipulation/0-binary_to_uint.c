62d2f5fba596d66d7fb71949d4e6a5a30dd6edd3
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-05-05 03:57:05 +0000

    bin to dec task 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * binary_to_unit - converts binary number to u_int
+ * @b: pointer to bin string
+ *
+ * Return: binary
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int b10 = 0;
+	int i;
+
+	if (b == NULL)
+		return (0);
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] > '1' || b[i] < '0')
+			return (0);
+		b10 = 2 * b10 + (b[i] - '0');
+	}
+	return (b10);
+}
