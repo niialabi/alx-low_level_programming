44ba4c6bb284991b8baebdd206a25e16d91424f8
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-05-09 16:35:32 +0000

    petty betty fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,9 +2,9 @@
 /**
  * create_file - creates file
  * @filename: name of file to create
- * text_content: contents
+ * @text_content: contents
  *
- * Return:1/ -1
+ * Return: 1/ -1
  */
 int create_file(const char *filename, char *text_content)
 {
@@ -19,7 +19,9 @@ int create_file(const char *filename, char *text_content)
 	if (!text_content)
 		return (1);
 	while (text_content[i] != '\0')
-	       i++;	
+	{
+		i++;
+	}
 	fd = write(fd, text_content, i);
 	if (fd < 0)
 		return (-1);
