0457bff101a4f649e87fc2f2fe7d5e1bb9cd0175
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-05-09 16:03:29 +0000

    permission fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_RDWR | O_TRUNC | O_CREAT);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (fd < 0)
 		return (-1);
 	if (!text_content)
