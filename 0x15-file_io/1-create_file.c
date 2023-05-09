48c8d9d09474032fcaa66bbf4d62bbcc16185b56
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-05-09 06:00:05 +0000

    bug fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_RDWR | O_APPEND | O_CREAT);
+	fd = open(filename, O_RDWR | O_TRUNC | O_CREAT);
 	if (fd < 0)
 		return (-1);
 	if (!text_content)
