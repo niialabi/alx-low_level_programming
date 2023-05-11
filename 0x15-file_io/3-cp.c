3d9c8afbf6a021db98a4210beb3f984bdfa14c29
Taufiq M. Alabi <taufiq.alabi@gmail.com>
2023-05-11 18:44:22 +0000

    cp task

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,62 @@
+#include "main.h"
+
+int main(int argc, char **argv)
+{
+	/*DECLARATIONS*/
+	int fd1, fd2;
+	char buffer[1024];
+	char *to = NULL, *from = NULL;
+	ssize_t read_from, write_to;
+
+
+	/*CODE*/
+	if (argc != 3)
+		error_print(97, from, to);
+	to = argv[2];
+	from = argv[1];
+	fd1 = open(from, O_RDONLY);
+	if (fd1 == -1)
+		error_print(98, from, to);
+	read_from = read(fd1, buffer, 1024);
+	if (read_from == -1)
+		error_print(98, from, to);
+	fd2 = open(to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd2 == -1)
+		error_print(99, from, to);
+	write_to = write(fd2, buffer, read_from);
+	if (write_to == -1)
+		error_print(99, from, to);
+	read_from = close(fd1);
+	if (read_from == -1)
+		error_print(100, from, to);
+	write_to = close(fd2);
+	if (write_to == -1)
+		error_print(100, from, to);
+
+
+
+	return (0);
+}
+/**
+ * error_print - prog to print 
+ *
+ */
+void error_print(int ret, char *from, char *to)
+{
+	switch (ret)
+	{
+		case 97:
+			dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+			 break;
+		case 98:
+			 dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", from);
+			 break;
+		case 99: 
+			 dprintf(STDERR_FILENO, "Error: Can't write to %s\n", to);
+			 break;
+		case 100: 
+			 dprintf(STDERR_FILENO, "Error: Can't close fd %s\n", to);
+			  break;
+	}
+	exit (ret);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -13,5 +13,7 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int _putchar(char c);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int cp(const char *file_from, const char *file_to);
+void error_print(int ret, char *from, char *to);
 
 #endif
