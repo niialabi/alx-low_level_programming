63f8bd30ffe306eab0d7bbd615b6bcbe3ae06096
niialabi <taufiq.alabi@gmail.com>
2023-06-18 13:29:50 +0000

    error check

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -11,20 +11,12 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *tmp = NULL;
 
-	tmp = malloc(sizeof(dlistint_t));
+	tmp = malloc(sizeof(struct dlistint_s));
 	if (tmp == NULL)
 		return (NULL);
 	tmp->n = n;
-	if (head == NULL)
-	{
-		tmp->next = NULL;
-		tmp->prev = NULL;
-		*head = tmp;
-	}
-	else
-	{
-		tmp->next = *head;
-		*head = tmp;
-	}
+	tmp->next = *head;
+	tmp->prev = NULL;
+	*head = tmp;
 	return (*head);
 }
