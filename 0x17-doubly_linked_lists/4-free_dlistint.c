6bb9d6f12cbd2be0f40a88fd1081a38024482b88
niialabi <taufiq.alabi@gmail.com>
2023-06-18 15:11:08 +0000

    free doubly linked list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * free_dlistint - frees dynamically alocated memory
+ * @head: head pointer
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp = NULL;
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+}
